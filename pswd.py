41e571c2f13b89f28d307d378d88592f3708cd6f
Adit Ghosh <blubrdpirts@gmail.com>
2024-09-30 10:34:21 +0530

    functional but unadjusted Skeleton

--- pswd.py
+++ pswd.py
@@ -4,7 +4,14 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.clock import Clock
+from kivy.uix.label import Label
 
+class Still(App):
+    def build(self):
+        Clock.schedule_once(self.stop, 3)
+        return Label(text='Password Manager', font_size = 72)
+    
 class MyLayout(Widget):
     def spinner_clicked(self, value):
         self.ids.text_label.text=f'No. of Symbol chosen: {value}'
@@ -23,4 +30,5 @@ class pswd(App):
         return MyLayout()
     
 if __name__ == '__main__':
+    Still().run()
     pswd().run()
