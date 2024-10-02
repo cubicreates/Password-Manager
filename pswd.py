f05ce44655693cd2fb4848188bd208903bf7b48d
Adit Ghosh <blubrdpirts@gmail.com>
2024-10-02 10:41:51 +0530

    inserting the brain and getting the motor function

--- pswd.py
+++ pswd.py
@@ -6,11 +6,6 @@ from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from kivy.uix.label import Label
 
-global symbol
-global number
-global capletter
-global smlleter
-global password
 
 class Still(App):
     def build(self):
@@ -18,74 +13,85 @@ class Still(App):
         return Label(text='Password Manager', font_size = 72)
     
 class MyLayout(Widget):
-    def spinner_clicked(self, value):
-        self.ids.text_label.text=f'No. of Symbol chosen: {value}'
-        symbol = value
+    global symbol
+    global number
+    global capletter
+    global smlleter
+    global password
+    global LI
     
+    LI =[2,2,2,2]
 
-    def spinner_n_clicked(self, value):
-        self.ids.text_label_n.text=f'No. of Number chosen: {value}'
-        number = value
+    def spinner_clicked(self, value1):
+        self.ids.text_label.text=f'No. of Symbol chosen: {value1}'
+        LI[0]=int(value1)
+   
+    def spinner_n_clicked(self, value2):
+        self.ids.text_label_n.text=f'No. of Number chosen: {value2}'
+        LI[1]=int(value2)
+
+    def spinner_c_clicked(self, value3):
+        self.ids.text_label_c.text=f'No. of Capital Letter \n chosen: {value3}'
+        LI[2]=int(value3)
     
-    def spinner_c_clicked(self, value):
-        self.ids.text_label_c.text=f'No. of Capital Letter \n chosen: {value}'
-        capletter = value
-    
-    def spinner_s_clicked(self, value):
-        self.ids.text_label_s.text=f'No. of Small Letters \n chosen: {value}'
-        smlletter = value
-    
-    def press(self):
-        def passmanager():
-            alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] #alphabets
-            sym = ['@','#','$','%','^','&','*','(',')','_','-','+','=','{','}','|',':',';','"','<',',','>','.','?'] #Symbols
-            mainpass = [] #main password
+    def spinner_s_clicked(self, value4):
+        self.ids.text_label_s.text=f'No. of Small Letters \n chosen: {value4}'
+        LI[3]=int(value4)
 
-            #getting the numbers
-            i=1
-            while i<=number:
-                num = str(randint(0,9))
-                mainpass.append(num)
-                i+=1
+    def passmanager(self):
+        alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] #alphabets
+        sym = ['@','#','$','%','^','&','*','(',')','_','-','+','=','{','}','|',':',';','"','<',',','>','.','?'] #Symbols
+        mainpass = [] #main password
 
-            # getting capital letters
-            i=1
-            while i<=capletter:
-                a1 = randint(0,25)
-                a = alpha[a1]
-                a = a.upper()
-                mainpass.append(a)
-                i+=1
+        number = LI[0]
+        symbol = LI[1]
+        capletter = LI[2]
+        smlleter = LI[3]
 
-            # getting small letters
-            i=1
-            while i<=smlleter:
-                b1 = randint(0,25)
-                b = alpha[b1]
-                b = b.lower()
-                mainpass.append(b)
-                i+=1
+        #getting the numbers
+        i=1
+        while i<=number:
+            num = str(randint(0,9))
+            mainpass.append(num)
+            i+=1
 
-            # getting symbols
-            i=1
-            while i<=symbol:
-                s1= randint(0,23)
-                s= sym[s1]
-                mainpass.append(s)
-                i+=1
+        # getting capital letters
+        i=1
+        while i<=capletter:
+            a1 = randint(0,25)
+            a = alpha[a1]
+            a = a.upper()
+            mainpass.append(a)
+            i+=1
 
-            # Shuffling to get the password
-            shuffle(mainpass)
+        # getting small letters
+        i=1
+        while i<=smlleter:
+            b1 = randint(0,25)
+            b = alpha[b1]
+            b = b.lower()
+            mainpass.append(b)
+            i+=1
 
-            # making the password
-            password=''.join(mainpass)
-            print(password)
-            return password
+        # getting symbols
+        i=1
+        while i<=symbol:
+            s1= randint(0,23)
+            s= sym[s1]
+            mainpass.append(s)
+            i+=1
+
+        # Shuffling to get the password
+        shuffle(mainpass)
+
+        # making the password
+        password=''.join(mainpass)
+        print(password)
+        return Label(text='Password Manager', font_size = 42)
+
+
         
-        return Label(text=passmanager(), font_size = 72)
-        
-
-               
+   
 
 class pswd(App):
     def build(self):
